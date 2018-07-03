765ba7f72c640f6967496dc10d31d7c725c96073
huanglei <1348310392@qq.com>
2018-07-03 09:14:02 +0800

    在Service中实现一个接口

---
+++ app-service/src/main/java/com/baizhi/service/UserService.java
@@ -0,0 +1,13 @@
+package com.baizhi.service;
+
+import com.baizhi.dao.UserDao;
+
+/**
+ * Created by Administrator on 2018/7/3.
+ */
+public class UserService implements UserDao{
+
+    public void queryAll() {
+        System.out.println("-------------------------------------------");
+    }
+}
